bdeafc0ffcdebc5008ee6ec17d2268d184db6d41
cheng <792702352@qq.com>
2017-10-18 12:50:19 +0800

    添加累加器API注解

--- src/main/java/org/cheng/day2/VisualAccumulator.java
+++ src/main/java/org/cheng/day2/VisualAccumulator.java
@@ -3,46 +3,62 @@ package org.cheng.day2;
 import edu.princeton.cs.algs4.StdDraw;
 
 /**
+ * 累加器（实现可视化）
  * @author cheng
  *         2017/10/17 22:38
  */
 public class VisualAccumulator {
 
-    private double total;
-    private int N;
+    /**
+     * 数据总数
+     */
+    private int n = 0;
+    /**
+     * 数据总值
+     */
+    private double sum = 0.0;
 
-    VisualAccumulator(int trials, double max) {
+    public VisualAccumulator() {
+    }
+
+    /**
+     * 初始化累加器
+     *
+     * @param trials 总值
+     * @param max    最大值
+     */
+    public VisualAccumulator(int trials, double max) {
         StdDraw.setXscale(0, trials);
         StdDraw.setYscale(0, max);
         StdDraw.setPenRadius(.005);
     }
 
     /**
-     * 添加一个新的数据值
+     * 将指定的数值添加到累加器
      *
      * @param val 添加的值
      */
     void addDataValue(double val) {
-        N++;
-        total += val;
+        n++;
+        sum += val;
         StdDraw.setPenColor(StdDraw.DARK_GRAY);
-        StdDraw.point(N, val);
+        StdDraw.point(n, val);
         StdDraw.setPenColor(StdDraw.RED);
-        StdDraw.point(N, total / N);
+        StdDraw.point(n, sum / n);
     }
 
     /**
-     * 所有数据的平均值
+     * 计算数据值的平均值
      *
      * @return 平均值
      */
     private double mean() {
-        return total / N;
+        return sum / n;
     }
 
     @Override
     public String toString() {
-        return "Mean (" + N + " values): "
+        return "Mean (" + n + " values): "
                 + String.format("%7.5f", mean());
     }
 }
