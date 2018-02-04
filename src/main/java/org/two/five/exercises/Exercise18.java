562af81b6a69bc63aa583ae2e261fba929748ab6
cheng <792702352@qq.com>
2018-02-04 23:44:59 +0800

    算法2.5节习题

---
+++ src/main/java/org/two/five/exercises/Exercise18.java
@@ -0,0 +1,10 @@
+package org.two.five.exercises;
+
+/**
+ * 2.5.18 强制稳定。编写一段能够将任意排序方法变得稳定的封装代码，创建一种新的数据类型作为键，
+ * 将键的原始索引保存在其中，并在调用sort()之后再恢复原始的键。
+ * @author cheng
+ *         2018/2/4 23:38
+ */
+public class Exercise18 {
+}
