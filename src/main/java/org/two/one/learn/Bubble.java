0cd4e198081d36aad522654196e138efdf7eb012
cheng <792702352@qq.com>
2018-02-07 16:14:48 +0800

    将do-while循环替换为while循环

--- src/main/java/org/two/one/learn/Bubble.java
+++ src/main/java/org/two/one/learn/Bubble.java
@@ -12,9 +12,9 @@ public class Bubble {
 
     public static void sort(Comparable[] arr) {
         int n = arr.length;
-        boolean swapped;
+        boolean swapped = true;
 
-        do {
+        while (swapped) {
             swapped = false;
             for (int i = 1; i < n; i++) {
                 if (arr[i - 1].compareTo(arr[i]) > 0) {
@@ -25,7 +25,7 @@ public class Bubble {
             // 优化，每一次Bubble Sort都将最大的元素放在了最后的位置
             // 所以下一次排序，最后的元素可以不再考虑
             n--;
-        } while (swapped);
+        }
     }
 
     private static void swap(Comparable[] arr, int i, int j) {
