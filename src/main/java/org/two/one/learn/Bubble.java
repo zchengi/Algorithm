383b3a7cab614433bf477a66f0278373e3bc8c95
cheng <792702352@qq.com>
2018-02-07 14:34:53 +0800

    冒泡排序优化

--- src/main/java/org/two/one/learn/Bubble.java
+++ src/main/java/org/two/one/learn/Bubble.java
@@ -10,27 +10,7 @@ import org.tool.SortTestHelper;
  */
 public class Bubble {
 
-    /**
-     * 优化版冒泡排序
-     * 将每次交换更改为赋值，和一次交换。
-     */
     public static void sort(Comparable[] arr) {
-        int n = arr.length;
-        while (n > 0) {
-            Comparable temp = arr[0];
-            int maxIndex = 0;
-            for (int i = 1; i < n; i++) {
-                if (temp.compareTo(arr[i]) < 0) {
-                    temp = arr[i];
-                    maxIndex = i;
-                }
-            }
-            arr[maxIndex] = arr[--n];
-            arr[n] = temp;
-        }
-    }
-
-    public static void sort2(Comparable[] arr) {
         int n = arr.length;
         boolean swapped;
 
